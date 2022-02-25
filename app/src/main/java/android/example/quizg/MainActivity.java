6d4ecd2524574f72c8fcf6582aba2396183e2736
anandrajsingh <ayushpratap253@gmail.com>
2022-02-26 00:08:23 +0530

    Added Next Button

--- app/src/main/java/android/example/quizg/MainActivity.java
+++ app/src/main/java/android/example/quizg/MainActivity.java
@@ -6,23 +6,52 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+
+    private Question[] questionBank = new Question[] {
+            new Question(R.string.question_Australia, true),
+            new Question(R.string.question_MidEast, true),
+            new Question(R.string.question_Ocean, true),
+            new Question(R.string.question_america, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_africa, true),
+    };
+
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        questionTextView = (TextView) findViewById(R.id.question_text_view);
+
+
+        nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1 ) % questionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
         trueButton = (Button) findViewById(R.id.trueButton);
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(MainActivity
+                        .this,R.string.correct_toast,Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.TOP, 0,0);
                 toast.show();
 
@@ -38,5 +67,10 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+    }
+
+    private void updateQuestion(){
+        int question = questionBank[currentIndex].getTextResId();
+        questionTextView.setText(question);
     }
 }
